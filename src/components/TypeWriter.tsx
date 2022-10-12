bbce31df9d0a39213ff1c7b72298aa0f933bd628
Orlein <pcj9024@gmail.com>
2022-10-12 15:47:46 +0900

    add direction indicator

--- src/components/TypeWriter.tsx
+++ src/components/TypeWriter.tsx
@@ -7,7 +7,11 @@ function TypeWriter(props: UseTypeWriterProps) {
 
   return (
     <>
-      <div>{state.isForwarding ? "정방향 진행" : "역방향 진행"}</div>
+      <div>
+        {state.isForwarding
+          ? "정방향 진행 >>>>>>>>>>>>"
+          : "역방향 진행 <<<<<<<<<<<<"}
+      </div>
       <h1>{displayedText}</h1>
     </>
   );
