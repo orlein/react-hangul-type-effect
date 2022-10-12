ae3c991d3304bf1120583bc16a77278556110329
Orlein <pcj9024@gmail.com>
2022-10-12 14:38:16 +0900

    update

--- src/components/TypeWriter.tsx
+++ src/components/TypeWriter.tsx
@@ -2,20 +2,15 @@ import React from "react";
 import * as Hangul from "hangul-js";
 import useTypeWriter, { UseTypeWriterProps } from "../hooks/useTypeWriter";
 
-export default function TypeWriter(props: UseTypeWriterProps) {
+function TypeWriter(props: UseTypeWriterProps) {
   const [displayedText, state] = useTypeWriter(props);
 
   return (
     <>
-      <div>
-        {props.texts.map((v) => (
-          <ul key={v}>{v}</ul>
-        ))}
-      </div>
-      <p />
-      <hr />
       <div>{state.isForwarding ? "정방향 진행" : "역방향 진행"}</div>
       <h1>{displayedText}</h1>
     </>
   );
 }
+
+export default React.memo(TypeWriter);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,15 @@ import TypeWriter from "../components/TypeWriter";
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
+  const texts = React.useMemo(
+    () => [
+      "첫번째 한글문장",
+      "두번째는 영어문장이 섞여있는데 This is English sentence",
+      "세번째는 그냥 또 한글문장",
+      "네번째는 그냥 lorem ipsum dolor sit amet 어쩌구저쩌구",
+    ],
+    []
+  );
   return (
     <main>
       <div>
@@ -10,13 +19,15 @@ export default function HomePage() {
         <p>Type Delay: 50 ms </p>
         <p>Delete Delay: 10 ms </p>
       </div>
+      <div>
+        {texts.map((v) => (
+          <ul key={v}>{v}</ul>
+        ))}
+      </div>
+      <p />
+      <hr />
       <TypeWriter
-        texts={[
-          "첫번째 한글문장",
-          "두번째는 영어문장이 섞여있는데 This is English sentence",
-          "세번째는 그냥 또 한글문장",
-          "네번째는 그냥 lorem ipsum dolor sit amet 어쩌구저쩌구",
-        ]}
+        texts={texts}
         pauseDelay={1000}
         typeDelay={50}
         deleteDelay={10}
